081caf8e50cc544899017e4e160971228572227b
gonzalt <37196222+gonzalt@users.noreply.github.com>
2019-09-30 16:39:04 -0400

    Completed Lab 2 - AR
    Completed the in class lab for Lab 2  - AR.

---
+++ lab-ar/Assets/MidAirPositioningScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidAirPositioningScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	public void DoInPlace()
+	{
+		Debug.Log("Placed Content!");
+	}
+
+
+
+
+
+}
